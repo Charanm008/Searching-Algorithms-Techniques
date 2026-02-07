1f1558c04c159b2ed0717f2e70c14704e7ac622f
Charan Manthena <charanmant8@gmail.com>
2026-02-07 10:48:20 +0530

    Refactor UniformBinarySearch with main method

--- UniformBinarySearch.java
+++ UniformBinarySearch.java
@@ -1,7 +1,7 @@
 public class UniformBinarySearch {
-    static int[] lookupTable = {1, 2, 4, 8, 16, 32, 64, 128};
-    static int[] stepSizeTable = {1, 2, 4, 8, 16, 32, 64, 128};
-    
+    static int[] lookupTable = { 1, 2, 4, 8, 16, 32, 64, 128 };
+    static int[] stepSizeTable = { 1, 2, 4, 8, 16, 32, 64, 128 };
+
     static int uniformBinarySearch(int[] array, int size, int target) {
         int low = 0;
         int high = size - 1;
@@ -13,7 +13,7 @@ public class UniformBinarySearch {
                 return mid;
             } else if (array[mid] < target) {
                 low = mid + 1;
-            } else { 
+            } else {
                 high = mid - 1;
             }
             step++;
@@ -22,5 +22,24 @@ public class UniformBinarySearch {
             }
         }
         return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21 };
+        int target = 13;
+        if (args.length > 0) {
+            try {
+                target = Integer.parseInt(args[0]);
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid argument, using default target: " + target);
+            }
+        }
+
+        int idx = uniformBinarySearch(arr, arr.length, target);
+        if (idx != -1) {
+            System.out.println("Found " + target + " at index " + idx);
+        } else {
+            System.out.println(target + " not found");
+        }
     }
 }
