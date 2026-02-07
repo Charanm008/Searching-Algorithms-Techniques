ae20301e40ac603bcdc3e1c0bdf9767074e4537f
Charan Manthena <charanmant8@gmail.com>
2026-02-07 10:53:53 +0530

    Implement main method for InterpolationSearch
    Added a main method to test the interpolation search.

--- InterpolationSearch.java
+++ InterpolationSearch.java
@@ -25,4 +25,23 @@ public class InterpolationSearch {
         }
         return -1;
     }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21 };
+        int target = 13;
+        if (args.length > 0) {
+            try {
+                target = Integer.parseInt(args[0]);
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid argument, using default target: " + target);
+            }
+        }
+
+        int idx = interpolationSearch(arr, target);
+        if (idx != -1) {
+            System.out.println("Found " + target + " at index " + idx);
+        } else {
+            System.out.println(target + " not found");
+        }
+    }
 }
